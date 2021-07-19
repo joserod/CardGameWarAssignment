0af9a95b52fb21bc7ae1acd9779752d5bdba2eea
joserod <joseantoniorod@live.com>
2021-07-19 10:38:12 -0400

    Add files via upload

---
+++ CardGameWar/CardGameWar/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using CardGameWar.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameWar
+{
+    public static class Extensions
+    {
+        public static void Enqueue(this Queue<Card> cards, Queue<Card> newCards)
+        {
+            foreach(var card in newCards)
+            {
+                cards.Enqueue(card);
+            }
+        }
+    }
+}
---
+++ CardGameWar/CardGameWar/MainMenu.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace CardGameWar
+{
+    class MainMenu
+    {
+        static int indexMainMenu = 0;
+
+        public static void mainMenu()
+        {
+            //Console.Clear();
+
+            List<string> menuItems = new List<string>()
+            {
+                "Play",
+                "Settings",
+                "Exit"
+            };
+
+            Console.CursorVisible = false;
+            while (true)
+            {
+                string selectedMenuItem = drawMainMenu(menuItems);
+                if (selectedMenuItem == "Play")
+                {
+                    // Play the game, or start the app
+                }
+                else if (selectedMenuItem == "Settings")
+                {
+                    /* Go to settings. Just copy this menu system 
+                    and change the list names for each setting you want.*/
+                }
+                else if (selectedMenuItem == "Exit")
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        public static string drawMainMenu(List<string> items)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (i == indexMainMenu)
+                {
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine(items[i]);
+                }
+                else
+                {
+                    Console.WriteLine(items[i]);
+                }
+                Console.ResetColor();
+            }
+
+            ConsoleKeyInfo ckey = Console.ReadKey();
+            if (ckey.Key == ConsoleKey.DownArrow)
+            {
+                if (indexMainMenu == items.Count - 1) { }
+                else { indexMainMenu++; }
+            }
+            else if (ckey.Key == ConsoleKey.UpArrow)
+            {
+                if (indexMainMenu <= 0) { }
+                else { indexMainMenu--; }
+            }
+            else if (ckey.Key == ConsoleKey.LeftArrow)
+            {
+                Console.Clear();
+            }
+            else if (ckey.Key == ConsoleKey.RightArrow)
+            {
+                Console.Clear();
+            }
+            else if (ckey.Key == ConsoleKey.Enter)
+            {
+                return items[indexMainMenu];
+            }
+            else
+            {
+                return "";
+            }
+
+            //Console.Clear();
+            return "";
+        }
+    }
+}
